895ea0561b17685e4c5726dd1ad1898ff9e67b07
Christen Garcia <christen.garcia@gmail.com>
2017-11-05 03:35:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,14 @@ class Dog
   def name
     @name
   end
-  def initialize(name = "Mutt")
+  def initialize(name)
     @name = name
     @breed = breed_name
   end
 
   def breed
     @breed
-    
+
   def breed(breed_name = "Mutt")
     @breed = breed_name
 end
